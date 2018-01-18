9846e232c765a2b1f901857f25889afb8e5b350d
Austin <joha0033@gmail.com>
2018-01-18 13:15:28 -0700

    nav bar

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<!-- eslint-disable -->
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="success">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">Echo3</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">Overview</b-nav-item>
+          <b-nav-item href="#">Features</b-nav-item>
+          <b-nav-item href="#">Specs</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+      </b-collapse>
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+
+<style lang="scss">
+@import './styles/custom-bootstrap.scss';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  // margin-top: 60px;
+}
+.navbar-toggler-icon {
+  color: black !important
+}
+</style>
