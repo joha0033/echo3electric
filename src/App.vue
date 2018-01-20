b9522b8050a02cedf71ed22cc91489650dfdf204
Austin <joha0033@gmail.com>
2018-01-20 13:38:34 -0700

    added header route

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <!-- eslint-disable -->
 <template>
   <div id="app">
-    <b-navbar toggleable="md" type="dark" variant="success">
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="#">Echo3</b-navbar-brand>
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-          <b-nav-item href="#">Overview</b-nav-item>
-          <b-nav-item href="#">Features</b-nav-item>
-          <b-nav-item href="#">Specs</b-nav-item>
-          <b-nav-item href="#">Contact</b-nav-item>
-        </b-navbar-nav>
-        <!-- Right aligned nav items -->
-      </b-collapse>
-    </b-navbar>
+    <app-header></app-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/shared/header';
+// import Footer from './components/Body/Main/Footer.vue'
 export default {
   name: 'App',
+
+  data() {
+    return {
+      title: 'Echo-3',
+    };
+  },
+  components: {
+    'app-header': Header,
+    // 'app-footer': Footer
+  },
 };
 </script>
 
---
+++ src/components/shared/header.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <b-navbar toggleable="md" type="dark" variant="success">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">Echo3</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#">Overview</b-nav-item>
+        <b-nav-item href="#">Features</b-nav-item>
+        <b-nav-item href="#">Specs</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="css">
+</style>
