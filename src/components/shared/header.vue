3372c0b2f7bca46c1c6e3657ef370f60db7f4d05
Austin <joha0033@gmail.com>
2018-01-21 18:08:55 -0700

    get quote button

--- src/components/shared/header.vue
+++ src/components/shared/header.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <b-navbar toggleable="md" type="dark" variant="success">
+  <b-navbar class="" toggleable="md" type="dark" variant="success">
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
     <b-navbar-brand href="#">Echo3</b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
@@ -8,6 +8,17 @@
         <b-nav-item href="#">Features</b-nav-item>
         <b-nav-item href="#">Specs</b-nav-item>
         <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <div class="container">
+
+        </div>
+        <div class="container">
+            <b-button size="lg" class="my-2 my-sm-0" type="submit">Get a Quote</b-button>
+        </div>
+        <div class="container">
+
+        </div>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -20,4 +31,5 @@ export default {
 </script>
 
 <style lang="css">
+
 </style>
